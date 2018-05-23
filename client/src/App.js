dbdf0d7663f7e3fac7fb01f347fe2ef987bd37ff
Zhenzhen You <swimreally@MacBook-Air.local>
2018-05-23 10:45:18 -0700

    still play with materialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Button, Card, Dropdown, Footer, Navbar, NavItem, Icon, Pagination, ProgressBar, Row, Col } from 'react-materialize';
+import { Button, Card, Col, Dropdown, Footer, Input, Navbar, NavItem, Icon, Pagination, ProgressBar, Row, SideNav, SideNavItem } from 'react-materialize';
 import './App.css';
-import D3Analysis from './d3.js';
+import D3 from './D3';
 
 // cannot use route '/data', seems to be a reserved word?
 const callApi = async (route) => {
@@ -19,6 +19,9 @@ class App extends Component {
     response: []
   }
 
+  // will only update the state when the page is first loaded.
+  // won't update the state when the page is being refresed.
+  // need to think an alternative way.
   UNSAFE_componentWillMount() {
     callApi('/states/2017')
     .then(res => {
@@ -59,13 +62,49 @@ class App extends Component {
             <NavItem>three</NavItem>
           </Dropdown>
         </Navbar>
+        <SideNav
+          trigger={<Button>SIDE NAV DEMO</Button>}
+          options={{ closeOnClick: true }}
+          >
+          <SideNavItem userView
+            user={{
+              background: 'img/office.jpg',
+              image: 'img/yuna.jpg',
+              name: 'John Doe',
+              email: 'jdandturk@gmail.com'
+            }}
+          />
+          <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
+          <SideNavItem href='#!second'>Second Link</SideNavItem>
+          <SideNavItem divider />
+          <SideNavItem subheader>Subheader</SideNavItem>
+          <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
+        </SideNav>
         <Button>Housing Price</Button>
         <Col m={6} s={12}>
             <Card className='blue-grey darken-1' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
             I am a very simple card.
             </Card>
         </Col>
-        <D3Analysis data={this.state.response} />
+        <Row>
+          <Input s={12} type='select' label="Materialize Select" defaultValue='2'>
+            <option value='1'>Option 1</option>
+            <option value='2'>Option 2</option>
+            <option value='3'>Option 3</option>
+          </Input>
+        </Row>
+        <Row>
+            <Input name='group1' type='checkbox' value='red' label='Red' />
+            <Input name='group1' type='checkbox' value='yellow' label='Yellow' defaultValue='checked' />
+            <Input name='group1' type='checkbox' value='green' label='Green' className='filled-in' defaultChecked='checked' />
+            <Input name='group1' type='checkbox' value='brown' label='Brown' disabled='disabled' />
+        </Row>
+        <Row>
+          <Input name='on' type='date' onChange={function(e, value) {}} />
+        </Row>
+
+        <D3 data={this.state.response} />
+
         <Row>
           <Col s={12}>
             <ProgressBar progress={70}/>
