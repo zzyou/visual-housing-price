6c7ab97b5a6419060905d97224dabcd36de9cfa5
Zhenzhen You <swimreally@MacBook-Air.local>
2018-05-29 22:42:15 -0700

    axes stay the same while bars change

--- client/src/Component/Chart.js
+++ client/src/Component/Chart.js
@@ -19,20 +19,23 @@ class Chart extends Component {
 
         const data = rawData.filter(d => d.yr === year);
 
+        const data2017 = rawData.filter(d => d.yr === 2017);
+
         const margins = { top: 50, right: 20, bottom: 100, left: 60 };
         const svgDimensions = { 
             width: Math.max(this.props.parentWidth, 700), 
             height: 650 
         };
         const maxValue = Math.max(...data.map(d => d.index_nsa));
+        const maxValue2017 = Math.max(...data2017.map(d => d.index_nsa));
 
         const xScale = this.xScale
             .padding(0.5)
-            .domain(data.map(d => d.place_id))
+            .domain(data2017.map(d => d.place_id))
             .range([margins.left, svgDimensions.width - margins.right]);
         
         const yScale = this.yScale
-            .domain([0, maxValue])
+            .domain([0, maxValue2017])
             .range([svgDimensions.height - margins.bottom, margins.top]);
         
         return (
