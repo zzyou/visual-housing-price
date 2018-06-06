7d30d3eab4ece8675efc4faebb98a8f8d0b31ca0
Zhenzhen You <swimreally@MacBook-Air.local>
2018-06-05 23:10:40 -0700

    add FetchData higher order component to wrap App and User

---
+++ client/src/FetchData.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+const FetchData = (url) => (Comp) =>
+  class FetchData extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        data: [],
+        isLoading: false,
+        error: null,
+      };
+    }
+
+    componentDidMount() {
+      this.setState({ isLoading: true });
+
+      fetch(url)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Something went wrong ...');
+          }
+        })
+        .then(data => this.setState({ data, isLoading: false }))
+        .catch(error => this.setState({ error, isLoading: false }));
+    }
+
+    render() {
+      return <Comp { ...this.props } { ...this.state } />
+    }
+  }
+
+export default FetchData;
